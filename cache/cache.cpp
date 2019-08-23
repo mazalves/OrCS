a52b904a46568bf643a6610ad453a0b3e723a255
Aline Santana Cordeiro <alinesantanacordeiro@gmail.com>
2019-08-23 16:21:05 -0300

    adjusting details in cache

--- cache/cache.cpp
+++ cache/cache.cpp
@@ -23,11 +23,11 @@ cache_t::cache_t() {
 	L1_DATA_ASSOCIATIVITY = orcs_engine.configuration->getSetting ("L1_DATA_ASSOCIATIVITY");
     L1_DATA_LATENCY = orcs_engine.configuration->getSetting ("L1_DATA_LATENCY");
 	L1_DATA_SETS = (L1_DATA_SIZE/LINE_SIZE)/L1_DATA_ASSOCIATIVITY;
-        
+
     L1_INST_ASSOCIATIVITY = orcs_engine.configuration->getSetting ("L1_INST_ASSOCIATIVITY");
     L1_INST_LATENCY = orcs_engine.configuration->getSetting ("L1_INST_LATENCY");
 	L1_INST_SETS = (L1_INST_SIZE/LINE_SIZE)/L1_INST_ASSOCIATIVITY;
-        
+
     L2_ASSOCIATIVITY = orcs_engine.configuration->getSetting ("L2_ASSOCIATIVITY");
     L2_LATENCY = orcs_engine.configuration->getSetting ("L2_LATENCY");
 	L2_SETS = (L2_SIZE/LINE_SIZE)/L2_ASSOCIATIVITY;
@@ -36,7 +36,7 @@ cache_t::cache_t() {
     LLC_ASSOCIATIVITY = orcs_engine.configuration->getSetting ("LLC_ASSOCIATIVITY");
     LLC_LATENCY = orcs_engine.configuration->getSetting ("LLC_LATENCY");
 	LLC_SETS = (LLC_SIZE/LINE_SIZE)/LLC_ASSOCIATIVITY;
-        
+
     PREFETCHER_ACTIVE = orcs_engine.configuration->getSetting ("PREFETCHER_ACTIVE");
 
 	INSTRUCTION_LEVELS = orcs_engine.configuration->getSetting ("INSTRUCTION_LEVELS");
@@ -56,7 +56,7 @@ cache_t::~cache_t(){
 
 // Allocate each cache type (removed EMC case)
 void cache_t::allocate(cacheId_t cache_type, uint32_t cache_level, uint32_t cache_size, uint32_t cache_associativity, uint32_t cache_latency) {
-    //printf("-> Allocating %u cache %u\n", cache_type, cache_level);
+    // printf("-> Allocating %u cache %u\n", cache_type, cache_level);
     this->id = cache_type;
     this->level = cache_level;
     this->size = cache_size;
@@ -87,8 +87,8 @@ void cache_t::allocate(cacheId_t cache_type, uint32_t cache_level, uint32_t cach
 }
 
 // Return address index in cache
-inline void cache_t::tagIdxSetCalculation(uint64_t address, uint32_t *idx, uint32_t *tag) {
-    //printf("%s\n", "-> tagIdxSetCalculation in cache.cpp");
+inline void cache_t::tagIdxSetCalculation(uint64_t address, uint32_t *idx, uint64_t *tag) {
+    // printf("%s\n", "-> tagIdxSetCalculation in cache.cpp");
 	uint32_t get_bits = (this->n_sets) - 1;
 	*tag = (address >> this->offset);
 	*idx = *tag & get_bits;
@@ -97,8 +97,9 @@ inline void cache_t::tagIdxSetCalculation(uint64_t address, uint32_t *idx, uint3
 // Reads a cache, updates cycles and return HIT or MISS status
 // TODO considering SandyBridge latency differences (3-6-9-...)
 uint32_t cache_t::read(uint64_t address,uint32_t &ttc){
-    //printf("%s\n", "-> read in cache.cpp");
-    uint32_t idx, tag;
+    // printf("%s\n", "-> read in cache.cpp");
+    uint32_t idx;
+    uint64_t tag;
 	this->tagIdxSetCalculation(address, &idx, &tag);
     //printf("    address: %lu | tag: %u | idx: %u | offset: %u\n", address, tag, idx, this->offset);
     //printf("    Searches tag in associativity\n");
@@ -149,7 +150,7 @@ uint32_t cache_t::read(uint64_t address,uint32_t &ttc){
 
 // Returns the minor LRU line
 inline uint32_t cache_t::searchLru(cacheSet_t *set) {
-    //printf("%s\n", "-> searchLru in cache.cpp");
+    // printf("%s\n", "-> searchLru in cache.cpp");
 	uint32_t index = 0;
 	for (uint32_t i = 1; i < set->n_lines; i++)	{
 		index = (set->lines[index].lru <= set->lines[i].lru)? index : i;
@@ -159,7 +160,7 @@ inline uint32_t cache_t::searchLru(cacheSet_t *set) {
 
 // Copy data information to lower cache levels when data addresses are valid
 void cache_t::copyLevels(line_t *line, uint32_t idxa, uint32_t idxb) {
-    //printf("%s\n", "    copy cache information to lower cache levels when data address is valid");
+    // printf("%s\n", "    copy cache information to lower cache levels when data address is valid");
 	if (line->line_ptr_caches[0][idxa]->dirty == 1) {
 		line->line_ptr_caches[0][idxa]->line_ptr_caches[0][idxb]->dirty = line->line_ptr_caches[0][idxa]->dirty;
 		line->line_ptr_caches[0][idxa]->line_ptr_caches[0][idxb]->lru = orcs_engine.get_global_cycle();
@@ -170,7 +171,8 @@ void cache_t::copyLevels(line_t *line, uint32_t idxa, uint32_t idxb) {
 
 // Copy data information to lower cache levels when data addresses are invalid
 void cache_t::copyNextLevels(line_t *line, uint32_t idx) {
-    //printf("%s\n", "    copy cache information to lower cache levels when data address is invalid");
+    printf("%u\n", line->dirty);
+    // printf("%s %u\n", "    copy cache information to lower cache levels when data address is invalid", idx);
 	line->line_ptr_caches[0][idx]->dirty = line->dirty;
 	line->line_ptr_caches[0][idx]->lru = orcs_engine.get_global_cycle();
 	line->line_ptr_caches[0][idx]->ready_at = line->ready_at;
@@ -178,7 +180,7 @@ void cache_t::copyNextLevels(line_t *line, uint32_t idx) {
 
 // Writebacks an address from a specific cache to its next lower level (removed EMC)
 inline void cache_t::writeBack(line_t *line) {
-    //printf("-> writeback in cache.cpp - cache level = %u\n", this->level);
+    // printf("-> writeback in cache.cpp - cache level = %u\n", this->level);
     //printf("%s\n", "    searches for line references in another caches");
     for (uint32_t i = this->level + 1; i < DATA_LEVELS - 1; i++) {
         ERROR_ASSERT_PRINTF(line->line_ptr_caches[0][i] != NULL, "Error, no line reference in next levels.")
@@ -186,7 +188,7 @@ inline void cache_t::writeBack(line_t *line) {
 
 	// L1 writeBack issues
 	if (this->level == 0) {
-        //printf("%s\n", "    in L1 level:");
+        // printf("%s\n", "    in L1 level:");
 		for (uint32_t i = 1; i < DATA_LEVELS; i++) {
 			this->copyNextLevels(line, i);
             //printf("%s\n", "    and NULLs information from other caches to this level");
@@ -206,17 +208,25 @@ inline void cache_t::writeBack(line_t *line) {
 
 	// Intermediate cache levels issues
 	} else {
-        //printf("%s\n", "    in intermediate levels:");
+        // printf("%s\n", "    in intermediate levels:");
 		uint32_t i;
 		for (i = 0; i < this->level - 1; i++) {
+            // printf("%s\n", "for");
 			if (line->line_ptr_caches[0][i] != NULL) {
+                // printf("%s\n", "if");
 				copyLevels(line, i, i + 1);
 			}
 		}
-		if (line->line_ptr_caches[0][i] != NULL) {
+        // printf("%u\n", i);
+        if (line->line_ptr_caches[0][i] != NULL) {
+            // printf("%s\n", "if");
 			copyLevels(line, i, i + 2);
 		} else {
-			copyNextLevels(line, i + 2);
+            // printf("%s\n", "else");
+			// copyNextLevels(line->line_ptr_caches[0][i], i + 2);
+            line->line_ptr_caches[0][i + 2]->dirty = line->dirty;
+            line->line_ptr_caches[0][i + 2]->lru = orcs_engine.get_global_cycle();
+            line->line_ptr_caches[0][i + 2]->ready_at = line->ready_at;
 		}
         //printf("%s\n", "    NULLs lines from higher level caches");
 		for (uint32_t i = this->level + 1; i < DATA_LEVELS; i++) {
@@ -230,9 +240,10 @@ inline void cache_t::writeBack(line_t *line) {
 
 // Searches for a cache line to write data (removed EMC)
 line_t* cache_t::installLine(uint64_t address, uint32_t latency) {
-    //printf("-> installLine in cache.cpp - cache level = %u\n", this->level);
+        // printf("-> installLine in cache.cpp - cache level = %u\n", this->level);
 	int32_t line = POSITION_FAIL;
-    uint32_t idx, tag;
+    uint32_t idx;
+    uint64_t tag;
     this->tagIdxSetCalculation(address, &idx, &tag);
     //printf("%s\n", "    searches for a valid line in cache");
 	for (size_t i = 0; i < this->sets->n_lines; i++) {
@@ -247,6 +258,7 @@ line_t* cache_t::installLine(uint64_t address, uint32_t latency) {
         //printf("%s\n", "There is no valid line in set");
 		line = this->searchLru(&this->sets[idx]);
         //printf("    line %u in set %u was found.\n", line, idx);
+        printf("%u\n", this->sets[idx].lines[line].line_ptr_caches[0][this->level+1]->dirty);
 		this->add_change_line();
 		if (this->sets[idx].lines[line].dirty == 1) {
             //printf("    line %u is dirty\n", line);
@@ -267,8 +279,9 @@ line_t* cache_t::installLine(uint64_t address, uint32_t latency) {
 
 // Selects a cache line to install an address and points this memory address with the other cache pointers
 void cache_t::returnLine(uint64_t address, cache_t *cache) {
-    //printf("-> returnLine in cache.cpp - cache level = %u\n", this->level);
-    uint32_t idx, tag;
+    // printf("-> returnLine in cache.cpp - cache level = %u\n", this->level);
+    uint32_t idx;
+    uint64_t tag;
     this->tagIdxSetCalculation(address, &idx, &tag);
 	int32_t line = POSITION_FAIL;
 
@@ -285,12 +298,14 @@ void cache_t::returnLine(uint64_t address, cache_t *cache) {
     ERROR_ASSERT_PRINTF(line != POSITION_FAIL, "Error, line não encontrada para retorno")
 
     //printf("%s\n", "chegou no problema");
+    // printf("this level: %d  cache level: %d \n", this->level, cache->level);
 	if (this->level > 0) {
         //printf("cache level da cache do parametro: %u\n", cache->level);
 		line_t *line_return = NULL;
 		line_return = cache->installLine(address, this->latency);
-        //printf("    line in returnLine = %d selected\n", line);
+        // printf("    line in returnLine = %d selected\n", line_return->dirty);
 		this->sets[idx].lines[line].line_ptr_caches[0][cache->level] = line_return;
+
         // printf("    ptr_cache[%u] = line_return: %p - %p\n", cache->level, line_return, this->sets[idx].lines[line].line_ptr_caches[0][cache->level]);
 		for (uint32_t i = this->level + 1; i < DATA_LEVELS; i++) {
 			line_return->line_ptr_caches[0][i] = this->sets[idx].lines[line].line_ptr_caches[0][i];
@@ -312,8 +327,9 @@ void cache_t::returnLine(uint64_t address, cache_t *cache) {
 // @address write address
 // ============================
 uint32_t cache_t::write(uint64_t address){
-    //printf("%s\n", "-> write in cache.cpp");
-    uint32_t idx, tag;
+    // printf("%s\n", "-> write in cache.cpp");
+    uint32_t idx;
+    uint64_t tag;
     this->tagIdxSetCalculation(address, &idx, &tag);
 	int32_t line = POSITION_FAIL;
 	this->add_cache_write();
@@ -330,7 +346,8 @@ uint32_t cache_t::write(uint64_t address){
         line = this->searchLru(&this->sets[idx]);
         this->add_change_line();
         if (this->sets[idx].lines[line].dirty == 1) {
-            //printf("    line = %d is dirty!\n", line);
+            // printf("    line = %d is dirty!\n", line);
+            // printf("%u\n", this->sets[idx].lines[line].line_ptr_caches[0][this->level+1]->dirty);
             this->writeBack(&this->sets[idx].lines[line]);
             this->add_cache_writeback();
         }
