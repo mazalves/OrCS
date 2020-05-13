4d816af1195d0d944e7ce7e19a828732bd933153
Sairo Santos <sairoraoni@gmail.com>
2020-05-13 15:20:50 -0300

    config fix

---
+++ config/config.hpp
@@ -0,0 +1,10 @@
+class configure_t {
+    private:
+        libconfig::Config cfg;
+
+    public:
+        configure_t();
+        ~configure_t();
+        void loadConfig();
+        libconfig::Setting &getConfig();
+};
